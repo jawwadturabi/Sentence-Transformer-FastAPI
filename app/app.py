f6b83aeb2e30a300c5f6fbaafec712c426e3bbeb
Muhammad Jawwad Turabi <jawwadturabi@gmail.com>
2024-10-22 04:37:57 +0500

    code refactor

--- app/app.py
+++ app/app.py
@@ -1,99 +1,25 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from sentence_transformers import SentenceTransformer, util
-import numpy as np
+from fastapi import FastAPI, HTTPException,Request
 from mangum import Mangum
 from processDocuments import lambda_handler
-import os
-
 
 app = FastAPI()
-
-###############################################################################
-#   Handler for AWS Lambda                                                    #
-###############################################################################
-model = None
-
-@app.on_event("startup")
-def startup_event():
-    global model
-    model = SentenceTransformer("all-MiniLM-L6-v2",cache_folder=os.getenv('HF_HOME', '/tmp'))
-
-
 handler = Mangum(app)
+
 @app.get("/")
 def get():
-    print("testing")
-    return "Hello World"
+    return "Hello from process document app"
 
 
 @app.post("/processDocument")
-def process_document():
+async def process_document(request: Request):
     try:
-        # Simuler l'événement S3 pour appeler lambda_handler
-        event_payload = {
-            "Records": [
-                {
-                    "s3": {
-                        "bucket": {"name": "primavera-bucket"},
-                        "object": {
-                            "key": "documents/Unilever/UNILEVER_ICA_2015_16_17.pdf"
-                        },
-                    }
-                }
-            ]
-        }
+        payload = await request.json()
 
-        # Appeler la fonction lambda_handler
-        result = lambda_handler(event_payload)
+        if "Records" not in payload:
+            raise HTTPException(status_code=400, detail="Invalid payload: 'Records' key missing")
+
+        result = lambda_handler(payload)
 
         return result
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error processing document: {e}")
-
-
-class SimilarityInput(BaseModel):
-    phraseEmbedding: list
-    embeddedChunks: list
-
-
-class TextChunks(BaseModel):
-    sentences: list
-
-
-def calculate_similarity(phrase_embedding, embedded_chunks):
-    try:
-        phrase_embedding_np = np.array(phrase_embedding)
-        embedded_chunks_np = np.array(embedded_chunks)
-        similarities = util.cos_sim(phrase_embedding_np, embedded_chunks_np)
-        best_match_idx = np.argmax(similarities)
-        return {
-            "chunk": embedded_chunks[best_match_idx],
-            "similarity": float(similarities[0][best_match_idx]),
-        }
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Error calculating similarity: {e}"
-        )
-
-
-def embed_sentences(sentences):
-    try:
-        embeddings = model.encode(sentences)
-        return embeddings
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error generating embeddings: {e}")
-
-
-@app.post("/embed")
-def get_embeddings(text_chunks: TextChunks):
-    if not text_chunks.sentences:
-        raise HTTPException(status_code=400, detail="No sentences provided")
-    embeddings = embed_sentences(text_chunks.sentences)
-    return {"embeddings": embeddings.tolist()}
-
-
-@app.post("/calculate_similarity")
-def get_similarity(input_data: SimilarityInput):
-    result = calculate_similarity(input_data.phraseEmbedding, input_data.embeddedChunks)
-    return result
---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+import boto3
+# Load environment variables
+load_dotenv()
+
+# MongoDB client
+MONGODB_URI = os.environ['MONGODB_URI']
+client = MongoClient(MONGODB_URI)
+db = client['primavera']  # Name of the MongoDB database
+documents_collection = db['documents']  # Collection to store the extracted texts
+chunks_collection = db['chunks']
+
+# Initialize the S3 client
+s3_client = boto3.client('s3')
+bucket_name = 'primavera-bucket'

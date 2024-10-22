e432074b9cb7baae3d01bc2f841b9e28290578fc
Muhammad Jawwad Turabi <jawwadturabi@gmail.com>
2024-10-22 22:26:17 +0500

    api working fine on lambda

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.12-slim AS build
 WORKDIR /app
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install sentence-transformers==3.2.0
 
 # Stage 2: Production
 FROM public.ecr.aws/lambda/python:3.12
--- app/app.py
+++ app/app.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from mangum import Mangum
-from embeddings import calculate_similarity
+from embeddings_utils import calculate_similarity
 
 app = FastAPI()
-
 handler = Mangum(app)
 
 @app.get("/")
 def get():
-    print("testing")
     return "Hello from calculate similarity app"
+
 
 class SimilarityInput(BaseModel):
     phraseEmbedding: list
     embeddedChunks: list
 
+
 @app.post("/calculate_similarity")
 def get_similarity(input_data: SimilarityInput):
     try:
         # Call the function to calculate similarity using semantic search
-        result = calculate_similarity(input_data.phraseEmbedding, input_data.embeddedChunks)
+        result = calculate_similarity(
+            input_data.phraseEmbedding, input_data.embeddedChunks
+        )
         return result
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error calculating similarity: {e}")
+        raise HTTPException(
+            status_code=500, detail=f"Error calculating similarity: {e}"
+        )
